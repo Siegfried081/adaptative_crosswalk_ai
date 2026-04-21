a3fa2494cba557d1fc3f101bd2c05b7ef158ba24
Victor <mozardo.comm@outlook.com>
2026-04-20 22:23:29 -0300

    chore: setup initial project structure and CI

---
+++ training/export_model.py
@@ -0,0 +1,6 @@
+def export_model():
+    print("Export model script placeholder")
+
+
+if __name__ == "__main__":
+    export_model()
---
+++ training/train.py
@@ -0,0 +1,6 @@
+def train():
+    print("Training script placeholder")
+
+
+if __name__ == "__main__":
+    train()
